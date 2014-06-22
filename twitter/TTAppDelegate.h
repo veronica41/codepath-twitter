a5e985e8d47c4a1bdfbe657942b5ac8711f44581
Veronica Zheng <zhengzhixing@gmail.com>
2014-06-21 18:20:14 -0700

    create project

---
+++ twitter/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  twitter
+//
+//  Created by Veronica Zheng on 6/21/14.
+//  Copyright (c) 2014 Veronica Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  twitter
+//
+//  Created by Veronica Zheng on 6/21/14.
+//  Copyright (c) 2014 Veronica Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
